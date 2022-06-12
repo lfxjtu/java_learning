9a8a940c6df6eec3a91ff87503e81bd566a96042
Fang Liu <lfxjtu@gmail.com>
2022-06-12 20:52:42 +1200

    add some test

--- src/test/java/WrapperClasses.java
+++ src/test/java/WrapperClasses.java
@@ -1,3 +1,15 @@
-public class WrapperClasses {
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
 
+public class WrapperClasses {
+    public static void main(String[] args) {
+        Boolean a = false;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.54;
+        String e = "Bro";
+
+        System.out.println(e.toUpperCase());
+
+    }
 }
