4d33a38899d3c4d315857086df7e5bb90f47b53e
Fang Liu <lfxjtu@gmail.com>
2022-07-10 11:15:30 +1200

    add Cat object

---
+++ src/main/java/JavaLessons/Cat.java
@@ -0,0 +1,11 @@
+package JavaLessons;
+
+public class Cat {
+    private int age;
+    private String name;
+
+
+    public void say(){
+        System.out.println("Meow Meow");
+    }
+}
--- src/test/java/JavaLessons/HowToThis.java
+++ src/test/java/JavaLessons/HowToThis.java
@@ -1,4 +1,7 @@
 package JavaLessons;
 
 public class HowToThis {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+    }
 }
