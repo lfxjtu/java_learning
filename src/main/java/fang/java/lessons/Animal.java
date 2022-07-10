b16f67122fe84a56007972873ffe589ae5bfb2f2
Fang Liu <lfxjtu@gmail.com>
2022-07-10 13:11:38 +1200

    create parent class Animal

---
+++ src/main/java/fang/java/lessons/Animal.java
@@ -0,0 +1,22 @@
+package fang.java.lessons;
+
+public class Animal {
+    private String name;
+    private String colour;
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
